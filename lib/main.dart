03c202a21019080ce75cb628d436acf3b4552d3b
Lucas Maciel <2008lucasa@gmail.com>
2025-06-08 16:37:33 -0300

    Criação do aplicativo

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '/models/cart.dart';
+import '/models/order_list.dart';
+import '/models/product_list.dart';
+import '/pages/cart_page.dart';
+import '/pages/orders_page.dart';
+import '/pages/product_detail_page.dart';
+import '/pages/products_overview_page.dart';
+import '/utils/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OrderList(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          fontFamily: 'Lato',
+        ),
+        routes: {
+          AppRoutes.home: (ctx) => const ProductsOverviewPage(),
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+          AppRoutes.cart: (ctx) => const CartPage(),
+          AppRoutes.orders: (ctx) => const OrdersPage(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
